5c669791b9fbcaaf642e2c25491d153011e5f072
Katherine Bailey <katherine@katbailey.net>
2014-03-11 11:00:17 -0400

    Issue #2215523: Personalize Elements module should support having a control 'do nothing' option

--- modules/personalize_elements/js/personalize_elements.js
+++ modules/personalize_elements/js/personalize_elements.js
@@ -8,8 +8,12 @@
       if (element == undefined) return;
       if (Drupal.personalizeElements.hasOwnProperty(element.variation_type) && typeof Drupal.personalizeElements[element.variation_type].execute === 'function') {
         var choices = Drupal.settings.personalize.option_sets[osid].options,  choiceNames = Drupal.settings.personalize.option_sets[osid].option_names, choiceIndex = choiceNames.indexOf(choice_name);
+        // Option Sets of type Personalize Elements have the concept of a control option,
+        // where no change should be made to the selector. We need to pass in whether this
+        // is the control option.
+        var isControl = choice_name == Drupal.settings.personalize_elements.controlOptionName;
         var selectedContent = choices[choiceIndex]['personalize_elements_content'];
-        Drupal.personalizeElements[element.variation_type].execute($option_set, selectedContent);
+        Drupal.personalizeElements[element.variation_type].execute($option_set, selectedContent, isControl, osid);
         Drupal.personalize.executorCompleted($option_set, choice_name, osid);
       }
     }
@@ -18,37 +22,60 @@
   Drupal.personalizeElements = {};
 
   Drupal.personalizeElements.replaceText = {
-    execute : function($selector, selectedContent) {
-      $selector.text(selectedContent);
+    controlText : {},
+    execute : function($selector, selectedContent, isControl, osid) {
+      // We need to keep track of how we've changed the element, if only
+      // to support previewing different options.
+      if (!this.controlText.hasOwnProperty(osid)) {
+        this.controlText[osid] = $selector.text();
+      }
+      if (isControl) {
+        $selector.text(this.controlText[osid]);
+      }
+      else {
+        $selector.text(selectedContent);
+      }
+
     }
   };
 
   Drupal.personalizeElements.addClass = {
-    addedClasses : [],
-    execute : function($selector, selectedContent) {
-      for (var i in this.addedClasses) {
-        if (this.addedClasses.hasOwnProperty(i)) {
-          $selector.removeClass(this.addedClasses.shift(i));
+    addedClasses : {},
+    execute : function($selector, selectedContent, isControl, osid) {
+      // We need to keep track of how we've changed the element, if only
+      // to support previewing different options.
+      if (!this.addedClasses.hasOwnProperty(osid)) {
+        this.addedClasses[osid] = [];
+      }
+      for (var i in this.addedClasses[osid]) {
+        if (this.addedClasses[osid].hasOwnProperty(i)) {
+          $selector.removeClass(this.addedClasses[osid].shift());
         }
       }
-      $selector.addClass(selectedContent);
-      this.addedClasses.push(selectedContent);
+      if (!isControl) {
+        $selector.addClass(selectedContent);
+        this.addedClasses[osid].push(selectedContent);
+      }
     }
   };
 
   Drupal.personalizeElements.appendHtml = {
-    execute : function($selector, selectedContent) {
-      var id = 'personalize-elements-append';
+    execute : function($selector, selectedContent, isControl, osid) {
+      var id = 'personalize-elements-append-' + osid;
       $('#' + id).remove();
-      $selector.append('<div id="' + id + '">' + selectedContent + '</div>');
+      if (!isControl) {
+        $selector.append('<span id="' + id + '">' + selectedContent + '</span>');
+      }
     }
   };
 
   Drupal.personalizeElements.prependHtml = {
-    execute : function($selector, selectedContent) {
-      var id = 'personalize-elements-prepend';
+    execute : function($selector, selectedContent, isControl, osid) {
+      var id = 'personalize-elements-prepend-' + osid;
       $('#' + id).remove();
-      $selector.prepend('<div id="' + id + '">' + selectedContent + '</div>');
+      if (!isControl) {
+        $selector.prepend('<span id="' + id + '">' + selectedContent + '</span>');
+      }
     }
   };
 
