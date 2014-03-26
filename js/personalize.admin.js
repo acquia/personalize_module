fc3182fa77ee1aebb4d20569475b328371b0a97f
Lisa Backer <lisa.backer@acquia.com>
2014-03-26 19:25:50 -0400

    CH-334: Update content variation field set display.

--- js/personalize.admin.js
+++ js/personalize.admin.js
@@ -24,7 +24,7 @@
     Drupal.personalize.admin.togglePersonalizeCollapse($container);
     event.preventDefault();
     event.stopImmediatePropagation();
-  }
+  };
 
   /**
    * Toggle the collapse/editing state for a collapsible admin container.
@@ -81,20 +81,20 @@
    * It should be shown regardless of collapsed state of the container.
    */
   Drupal.behaviors.personalizeAdminContentHeader = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       $('.personalize-admin-content-header', context).once().each(function() {
         var $container = $(this).parents('.personalize-collapsible');
         $container.find('.personalize-admin-content-title').after('<div class="personalize-admin-content-title-suffix"></div>');
         $('.personalize-admin-content-title-suffix', $container).append($(this));
       });
     }
-  }
+  };
 
   /**
    * Handle show/hide of optional admin information.
    */
   Drupal.behaviors.personalizeAdminOptional = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       $('.personalize-admin-optional', context).once().each(function() {
         var closedText = Drupal.t('Info'),
           openedText = Drupal.t('Hide info'),
@@ -113,6 +113,36 @@
         $optional.hide();
       });
     }
-  }
+  };
+
+  /**
+   * Add any summary data to administrative fieldsets.
+   */
+  Drupal.behaviors.personalizeAdminFieldset = {
+    attach: function (context, settings) {
+      $('#personalize-agent-option-sets-form .personalize-admin-content-content fieldset').once().each(function() {
+        var fieldset = this,
+          $summary = $('.fieldset-legend:first .summary', this),
+          $summary_text = $('.fieldset-wrapper:first .personalize-summary', this);
+        if ($summary_text.length == 0) {
+          return;
+        }
+        if ($summary.length == 0) {
+          $summary = $('.fieldset-legend').append('<span class="summary"></span>');
+        }
+        $summary.html($summary_text.html());
+        $summary_text.hide();
+
+        var editText = $('.fieldset-wrapper .personalize-option-set-edit-trigger', fieldset).text(),
+          hideText = Drupal.t('hide');
+        $('.fieldset-wrapper .personalize-option-set-edit-trigger', fieldset).bind('click', function(e) {
+          e.preventDefault();
+          $('input.personalize-variation-winner', fieldset).toggleClass('personalize-edit');
+          var currentText = $(this).text();
+          $(this).text(currentText == editText ? hideText : editText);
+        });
+      });
+    }
+  };
 
 })(jQuery);
