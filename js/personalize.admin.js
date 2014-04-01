659d2f31e5b3b5fb710868c682c734e95d7cfbeb
Lisa Backer <lisa.backer@acquia.com>
2014-04-01 15:56:47 -0400

    Issue #2230209 by eshta: follow-up usability enhancements for campaign edit page.

--- js/personalize.admin.js
+++ js/personalize.admin.js
@@ -13,10 +13,6 @@
    */
   Drupal.personalize.admin.toggleClickHandler = function (event) {
     var $container = [];
-    // Ignore any clicks from the links in the title suffix.
-    if ($(event.target).parents('.personalize-admin-content-title-suffix').length > 0) {
-      return;
-    }
     if ($(event.target).hasClass('personalize-collapsible')) {
       $container = $(event.target);
     } else {
@@ -25,7 +21,13 @@
     if ($container.length == 0) {
       return;
     }
-    Drupal.personalize.admin.togglePersonalizeCollapse($container);
+    // Any clicks from links in the title suffix will expand the container.
+    if ($(event.target).parents('.personalize-admin-content-title-suffix').length > 0) {
+      Drupal.personalize.admin.togglePersonalizeCollapse($container, true);
+    } else {
+      // Otherwise just toggle.
+      Drupal.personalize.admin.togglePersonalizeCollapse($container);
+    }
     event.preventDefault();
     event.stopImmediatePropagation();
   };
@@ -35,11 +37,17 @@
    *
    * @param $container
    *   The jQuery container to toggle.
+   * @param open
+   *   (Optional) boolean indicating if the container should become open.
    */
-  Drupal.personalize.admin.togglePersonalizeCollapse = function ($container) {
+  Drupal.personalize.admin.togglePersonalizeCollapse = function ($container, open) {
     var closedText = Drupal.t('edit'),
       openText = Drupal.t('close'),
       label = $container.hasClass('personalize-collapsed') ? openText : closedText;
+    if (open === true && !$container.hasClass('personalize-collapsed')) {
+      // The container is already open.
+      return;
+    }
     $container.toggleClass('personalize-collapsed');
     $container.children('personalize-collapse-edit').text(label);
     if ($container.hasClass('personalize-collapsed')) {
@@ -144,10 +152,29 @@
           hideText = Drupal.t('hide');
         $('.fieldset-wrapper .personalize-option-set-edit-trigger', fieldset).bind('click', function(e) {
           e.preventDefault();
-          $('input.personalize-variation-winner', fieldset).toggleClass('personalize-edit');
+          $('div.personalize-variation-winner', fieldset).toggleClass('personalize-edit');
           var currentText = $(this).text();
           $(this).text(currentText == editText ? hideText : editText);
         });
+      });
+    }
+  };
+
+  /**
+   * Scroll to a new goal that has just been added to the admin page.
+   */
+  Drupal.behaviors.personalizeGoalAdded = {
+    attach: function (context, settings) {
+      var $newGoal = $('#personalize-agent-goals-form .personalize-goal-add', context).last();
+      if ($newGoal.length == 0) {
+        return;
+      }
+      var offset = $newGoal.offset();
+      var offsetTop = offset.top + 100; // scroll to just above the new goal
+      $('html, body').animate({
+        scrollTop: offsetTop
+      }, 1000, function() {
+        $newGoal.find('select').first().focus();
       });
     }
   };
