1a41c35281ae20914a2a936028f50f5e4c80e417
Lisa Backer <lisa.backer@acquia.com>
2014-04-23 15:32:53 -0400

    CH-409: Update the default option label from the selected block or the new block description.

--- modules/personalize_blocks/js/personalize_blocks.admin.js
+++ modules/personalize_blocks/js/personalize_blocks.admin.js
@@ -1,6 +1,31 @@
 (function ($) {
 
   Drupal.personalizeBlocks = Drupal.personalizeBlocks || {};
+
+  Drupal.personalizeBlocks.setOptionLabelFromExistingBlock = function (event) {
+    var $optionLabel = $(event.currentTarget).parents('tr').find('.personalize-blocks-add-option-label');
+    // Text is displayed in the format "Friendly (module:machine)".
+    var blockRe = /\s\(([a-zA-Z]|\d)+:([a-zA-Z]|\d)+\)$/;
+    var newLabel = $(event.currentTarget).find('option:selected').text().replace(blockRe, '');
+    Drupal.personalizeBlocks.setOptionLabel($optionLabel, newLabel);
+  };
+
+  Drupal.personalizeBlocks.setOptionLabelFromNewBlock = function (event) {
+    var $optionLabel = $(event.currentTarget).parents('tr').find('.personalize-blocks-add-option-label');
+    Drupal.personalizeBlocks.setOptionLabel($optionLabel, $(event.currentTarget).val());
+  };
+
+  Drupal.personalizeBlocks.setOptionLabel = function ($optionLabel, newLabel) {
+    var maxOptionLabelLength = 20;
+    if (Drupal.personalizeBlocks.changedOptionLabels.indexOf($optionLabel.attr('id')) >= 0) {
+      // The user has already changed the label.
+      return;
+    }
+    // Limit the text used as the label.
+    var truncateLength = Math.min(maxOptionLabelLength, newLabel.length);
+    $optionLabel.val(newLabel.substring(0, truncateLength));
+  }
+
   /**
    * Handles setting the initial value for option labels as blocks are selected.
    *
@@ -13,15 +38,21 @@
 
       // Update the appropriate option label when a block is selected so long
       // as the user has not yet changed the label.
-      $('.personalize-blocks-add-block-select', context).once().change(function(e) {
-        var $optionLabel = $(this).parents('tr').find('.personalize-blocks-add-option-label');
-        if (Drupal.personalizeBlocks.changedOptionLabels.indexOf($optionLabel.attr('id')) >= 0) {
-          // The user has already changed the label.
-          return;
+      $('.personalize-blocks-add-block-select', context).on('change', function(e) {
+        if ($(this).val() === 'add') {
+          // Take the option label from the new block info field.
+          var $optionLabel = $(event.currentTarget).parents('tr').find('.personalize-blocks-add-option-label');
+          $addInfo = $(this).closest('td').find('.personalize-blocks-add-block-info');
+          // Set the the default to the current text in the add block field.
+          Drupal.personalizeBlocks.setOptionLabel($optionLabel, $addInfo.val());
+          // Set a listener for any changes to that text.
+          $addInfo.on('keyup', Drupal.personalizeBlocks.setOptionLabelFromNewBlock);
+        } else {
+          // Take the option label from the selected block and stop listening
+          // to changes in the add block info field.
+          $(this).closest('td').find('.personalize-blocks-add-block-info').off('keyup', Drupal.personalizeBlocks.setOptionLabelFromNewBlock);
+          Drupal.personalizeBlocks.setOptionLabelFromExistingBlock(e);
         }
-        // Text is displayed in the format "Friendly (module:machine)".
-        var blockRe = /\s\(([a-zA-Z]|\d)+:([a-zA-Z]|\d)+\)$/;
-        $optionLabel.val($(this).find('option:selected').text().replace(blockRe, ''));
       });
 
       // Once the user updates the option label it should no longer be changed
@@ -30,6 +61,6 @@
         Drupal.personalizeBlocks.changedOptionLabels.push($(this).attr('id'));
       });
     }
-  }
+  };
 
 })(jQuery);
