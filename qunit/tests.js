1a163e5923927e67734d28c484851593e43a21b7
Katherine Bailey <katherine@katbailey.net>
2014-08-25 20:57:55 -0700

    Fixing existing tests and adding qunit tests

--- qunit/tests.js
+++ qunit/tests.js
@@ -116,7 +116,23 @@ QUnit.asyncTest( "get visitor contexts test", function( assert ) {
 });
 
 QUnit.test( "executor test", function( assert ) {
-  var $option_set = $('.personalize-osid-1');
-  Drupal.personalize.executors.show($option_set, 'first-choice', 1);
+  // Test the executor for a regular option set.
+  assert.equal(0, $('.osid-1-first-option').length);
+  assert.equal(0, $('.osid-1-second-option').length);
+  Drupal.personalize.executors.show.execute($('.personalize-osid-1'), 'first-choice', 1);
+  assert.equal(1, $('.osid-1-first-option').length);
+  assert.equal(0, $('.osid-1-second-option').length);
+  Drupal.personalize.executors.show.execute($('.personalize-osid-1'), 'second-choice', 1);
+  assert.equal(0, $('.osid-1-first-option').length);
+  assert.equal(1, $('.osid-1-second-option').length);
 
+  // Test an option set that appears multiple times on the page.
+  assert.equal(0, $('.osid-2-first-option').length);
+  assert.equal(0, $('.osid-2-second-option').length);
+  Drupal.personalize.executors.show.execute($('.personalize-osid-2'), 'first-choice', 2);
+  assert.equal(2, $('.osid-2-first-option').length);
+  assert.equal(0, $('.osid-2-second-option').length);
+  Drupal.personalize.executors.show.execute($('.personalize-osid-2'), 'second-choice', 2);
+  assert.equal(0, $('.osid-2-first-option').length);
+  assert.equal(2, $('.osid-2-second-option').length);
 });
