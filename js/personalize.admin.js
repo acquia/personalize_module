873a0f78ad8c19ec52680d99342aeda5729faec4
Lisa Backer <lisa.backer@acquia.com>
2015-02-03 12:54:49 -0500

    Remove old campaign page content.

--- js/personalize.admin.js
+++ js/personalize.admin.js
@@ -6,7 +6,6 @@
   /**
    * Campaign edit page functionality.
    *
-   * Make personalize admin content containers collapsible.
    * Adds add in context link for goals.
    */
   Drupal.behaviors.personalizeCampaignEdit = {
@@ -19,64 +18,6 @@
             document.location.href = val;
           }
         })
-      });
-      $('.personalize-admin-content', context).once(function() {
-        // If the fieldset is collapsed, then set this style to be collapsed.
-        var $holder = $(this);
-        var $fieldset = $holder.children('fieldset');
-        if (!$fieldset) {
-          return;
-        }
-        if (!$fieldset.hasClass('collapsible')) {
-          return;
-        }
-        if ($fieldset.hasClass('collapsed')) {
-          $holder.addClass('personalize-collapsed');
-        }
-        // When the fieldset title is clicked, then add toggle the collapsed class.
-        $('legend .personalize-fieldset-header .fieldset-title', $fieldset).on('click', function(e) {
-          $holder.toggleClass('personalize-collapsed');
-        });
-      });
-      $('.personalize-admin-content-header', context).once(function() {
-        var $container = $(this).parents('.personalize-collapsible');
-        $container.find('.personalize-admin-content-title').after('<div class="personalize-admin-content-title-suffix"></div>');
-        $('.personalize-admin-content-title-suffix', $container).append($(this));
-      });
-    }
-  };
-
-  /**
-   * Campaign edit form submission handling.
-   *
-   * Adds a confirmation prompt to the user when they are about to make changes
-   * to a running campaign that will result in pausing the campaign.
-   */
-  Drupal.behaviors.personalizeCampaignEditFormHandling = {
-    attach: function (context, settings) {
-      // Add a handler to form submits that trigger campaign status changes.
-      $('input.personalize-admin-warn', context).once().each(function() {
-        var currentCampaign = Drupal.settings.personalize.activeCampaign;
-        if (typeof currentCampaign === 'undefined') {
-          return;
-        }
-        // Overwrite beforeSubmit for each submit button (no cancel).
-        Drupal.ajax[this.id].options.beforeSubmit = function(form_values, $element, options) {
-          var campaign = Drupal.settings.personalize.campaigns[currentCampaign];
-          if (typeof(campaign) === 'undefined') {
-            return;
-          }
-          if (Drupal.settings.personalize.status && Drupal.settings.personalize.status[campaign.status]) {
-            var currentStatus = Drupal.settings.personalize.status[campaign.status];
-            if (currentStatus === Drupal.t('Running')) {
-              if (confirm(Drupal.t('Making these changes will pause the currently running campaign.  Are you sure you want to continue?'))) {
-                return true;
-              } else {
-                return false;
-              }
-            }
-          }
-        }
       });
     }
   };
@@ -102,23 +43,6 @@
           return false;
         });
         $optional.hide();
-      });
-    }
-  };
-
-  /**
-   * Add any summary data to administrative fieldsets.
-   */
-  Drupal.behaviors.personalizeAdminFieldset = {
-    attach: function (context, settings) {
-      $('#personalize-agent-option-sets-form .fieldset-wrapper fieldset').once().each(function() {
-        var fieldset = this,
-          $summary_text = $('.fieldset-wrapper:first .personalize-summary', this);
-        if ($summary_text.length == 0) {
-          return;
-        }
-        $('a.fieldset-title:first', this).after('<div class="personalize-admin-content-title-suffix">' + $summary_text.html() + '</div>');
-        $summary_text.remove();
       });
     }
   };
