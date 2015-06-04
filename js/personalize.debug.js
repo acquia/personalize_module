e69c61bd5cf6e7a039c292176d767a105e0fcd18
Lisa Backer <lisa.backer@acquia.com>
2015-06-04 14:37:24 -0400

    Update debug to log to local storage.

--- js/personalize.debug.js
+++ js/personalize.debug.js
@@ -1,18 +1,34 @@
-(function ($, Drupal) {
+(function ($, Drupal, Storage) {
 
   Drupal.personalizeDebug = (function() {
 
-    function getMessageType(code) {
+    function generateUUID(){
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+      });
+      return uuid;
+    };
+
+    function getSeverity(code) {
       if (code < 3000) {
-        return 'OK';
+        return 'info';
       }
       if (code < 4000) {
-        return 'WARNING';
+        return 'warning';
       }
-      return 'ERROR';
-    }
+      return 'error';
+    };
 
     var debuggedMessages = [];
+
+    function writeToStorage(data) {
+      var key = 'personalize::debug::' + generateUUID();
+      Storage.write(key, data, false);
+      return key;
+    };
 
     return {
       /**
@@ -33,8 +49,28 @@
           console.log(messageType + ': ' + message + ' [Code ' + code + ']');
           debuggedMessages.push(message);
         }
+
+        var severity = getSeverity(code);
+        var data = {
+          type: 'log',
+          timestamp: new Date().getTime(),
+          page: Drupal.settings.basePath + Drupal.settings.pathPrefix + Drupal.settings.visitor_actions.currentPath,
+          message: message,
+          severity: severity,
+          resolution: ''
+        };
+        // Write to local storage
+        var key = writeToStorage(data);
+
+        // Dispatch an event to alert the debugger that new stream data is
+        // available.
+        $(document).trigger('acquiaLiftDebugEvent', {
+          'key': key
+        });
+
+        // Save to request tracking for duplicates.
+        debuggedMessages.push(message);
       }
     };
   })();
-
-})(Drupal.jQuery, Drupal);
+})(Drupal.jQuery, Drupal, Drupal.personalizeStorage);
